1d9b2eb9b2ed79ca4b68ad8719da0151c2e1a42b
akradore <akradore@gmail.com>
2021-01-26 18:22:51 +0200

    [UPD] Updated Pmt functions

--- base_palucron/model/loan_calculations.py
+++ base_palucron/model/loan_calculations.py
@@ -7,12 +7,12 @@ import numpy
 class CRMLead(models.Model):
     _inherit = 'crm.lead'
 
-    def some_function(self, rate, insta_num,terms,principle):
-        return (0 - numpy.ipmt(rate/12,insta_num,terms,principle))
+    def calc_function_ipmt(self, rate, insta_num,terms,principal):
+        return (0 - numpy.ipmt(rate/12,insta_num,terms,principal))
     
-    def some_function_ppmt(self, rate, insta_num,terms,principle):
-        return (0 - numpy.ppmt(rate/12,insta_num,terms,principle))
+    def calc_function_ppmt(self, rate, insta_num,terms,principal):
+        return (0 - numpy.ppmt(rate/12,insta_num,terms,principal))
     
-
-
+    def calc_function_pmt(self, rate, insta_num,terms,principal):
+        return (0 - numpy.pmt(rate/12,terms,principal))
     
